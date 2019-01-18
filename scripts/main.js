369380b4f378c9b0d12da0e5a15e4e40a5f30a71
Ash <lou.mosnier@gmail.com>
2019-01-18 10:48:38 +0100

    textarea

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 /* eslint-disable */
+var app = new Vue({
+    el: '#app',
+    data: {
+      todos: [
+        { text: '', done: false },
+      ],
+      newTodo: '',
+      dateSave: '',
+      displayDone: true
+    },
+    methods: {
+        addNewTodo: function () {
+            this.todos.push({text: this.newTodo, done: false})
+            this.newTodo = ''
+            this.dateSave = 'last updated ' + new Date().toLocaleString()
+        },
+        getDoneCount: function () {
+            var doneCount = 0
+            for (var todo of this.todos) {
+                if (todo.done) {
+                    doneCount++
+                }
+            }
+            return doneCount
+        }
+    }
+  })

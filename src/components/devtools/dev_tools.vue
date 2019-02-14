eefed694427a7cd6782947373163fb6030c610ca
Ash <lou.mosnier@gmail.com>
2019-02-14 11:30:20 +0100

    devtools

--- src/components/devtools/dev_tools.vue
+++ src/components/devtools/dev_tools.vue
@@ -1,13 +1,56 @@
 <template>
-    <button>Réinitialiser les données de développement</button>
+    <div>
+        <button @click="resetData">Réinitialiser les données de développement</button>
+        <h1>Données actuelles</h1>
+        <textarea v-model="currentData"></textarea>
+    </div>
 </template>
 
 <script>
+import store from '@/store'
+import devData from '@/dev_data'
+import Note from '@/models/note'
+import Contact from '@/models/contact'
+import Project from '@/models/project'
+
 export default {
-    name: 'DevTools'
+    name: 'DevTools',
+    methods: {
+        resetData: function () {
+            store.contacts.length = 0
+            devData.contacts.forEach(function (data) {
+                var contact = new Contact(data)
+                contact.saveToLocalStorage()
+            })
+
+
+            store.notes.length = 0
+            devData.notes.forEach(function (data) {
+                var note = new Note(data)
+                note.saveToLocalStorage()
+            })
+
+
+            store.projects.length = 0
+            devData.projects.forEach(function (data) {
+                var project = new Project(data)
+                project.saveToLocalStorage()
+            })
+
+        }
+    },
+    computed: {
+        currentData: function () {
+            return JSON.stringify(store, null, 4)
+        }
+    }
 }
 </script>
 
 
-<style>
+<style scoped>
+textarea {
+    width: 60%;
+    height: 500px;
+}
 </style>
